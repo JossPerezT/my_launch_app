1a5dde88ad0d58ef3cc6fdc36289611707519b01
JossPerezT <jossperez44@gmail.com>
2022-04-26 23:32:20 -0500

    3.4 Importando la clase MissionComander, instanciando 3 objetos e imprimiendo los atributos name de cada uno

---
+++ index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require(`./app/missionComander`);
+
+const carlo = new MissionCommander ("Carlo Gilmar")
+const fer = new MissionCommander ("Fernanda Ochoa")
+const rodrigo = new MissionCommander ("Rodrigo Martinez")
+
+
+console.log (carlo.name)
+console.log (fer.name)
+console.log (rodrigo.name)
