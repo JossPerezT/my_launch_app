b9c7e9a42f95ce3afdc5b2ae2946d080aea602ee
JossPerezT <jossperez44@gmail.com>
2022-04-26 23:53:38 -0500

    4.4.1 Corrigiendo la prueba de unidad desde .toBe()

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -1,6 +1,6 @@
 describe ("Esto es una suite de prubeas ", () => {
     test (`Caso de prueba 1`, () => {
-        const result = 1+9
-        expect (result).toBe (10);
+        const result = 1+2
+        expect (result).toBe (3);
     })
 })
