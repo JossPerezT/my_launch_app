e2c4bb38b3eebcf2bc5b781a8d704edccefd2830
JossPerezT <jossperez44@gmail.com>
2022-04-27 00:09:04 -0500

    4.6 Corrigiendo la prueba de unidad

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -2,7 +2,7 @@ const MissionCommander = require(`./../app/missionComander`)
 
 describe ("1. Prueba de unidad para la clase de MissionCommander ", () => {
     test (`Caso de prueba 1: Creación de un objeto MissionCommander`, () => {
-        const myMissionCommander = new MissionCommander ("Woopa!!")
+        const myMissionCommander = new MissionCommander ("Woopa")
         expect (myMissionCommander).toBe ("Woopa");
     })
 })
