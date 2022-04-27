d40a0b368e09f0e2d669788fe90f6f1fb60fe8f6
JossPerezT <jossperez44@gmail.com>
2022-04-27 00:14:52 -0500

    4.7 Corriguiendo la prueba de unidad. La prueba ha pasado. Fin de la misión 1 'Creación de proyectos JS' de la semana 3

--- test/missionComander.test.js
+++ test/missionComander.test.js
@@ -3,6 +3,6 @@ const MissionCommander = require(`./../app/missionComander`)
 describe ("1. Prueba de unidad para la clase de MissionCommander ", () => {
     test (`Caso de prueba 1: Creación de un objeto MissionCommander`, () => {
         const myMissionCommander = new MissionCommander ("Woopa")
-        expect (myMissionCommander).toBe ("Woopa");
+        expect (myMissionCommander.name).toBe ("Woopa");
     })
 })
